79dac80cd1efa193ba4d967ef34fa995145377e5
Anna Fischer <fischer@fastec.de>
2015-06-02 08:28:57 +0200

    Added missing .length

--- massautocomplete.js
+++ massautocomplete.js
@@ -145,7 +145,7 @@ angular.module('MassAutoComplete', [])
                 // to return to his original expression after suggestions were made.
                 $scope.results = [{ value: term, label: '' }].concat(suggestions);
                 $scope.selection = term.length > 0 && suggestions.length > 0 ? suggestions[0].value : '';
-                if ($scope.selection === term && term > 0) {
+                if ($scope.selection === term && term.length > 0) {
                   // The auto_complete list will be closed if selection and term are equal and not empty.
                   $scope.show_autocomplete = false;
                 }
